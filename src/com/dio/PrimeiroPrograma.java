9374c5861eca9295b2469564d564d0de418f2652
thaisquirino04 <thaisquirino0402@gmail.com>
2022-07-08 23:45:32 -0300

    first commit

---
+++ src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,26 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Gato gato = new Gato();
+		Livros Livros = new Livros();
+		
+		System.out.println(gato);
+		System.out.print(Livros);
+		
+		/*int a = 2;
+		int b = 3;
+		System.out.println("Hello World " + (a+b));*/
+	}
+
+}
+
+class Livros {
+	private String nome;
+	private String npag;
+}
